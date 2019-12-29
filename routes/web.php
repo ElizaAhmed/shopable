78963605310146f4656d2beec8546d088d516c12
Eliza Ahmed <elizarafee@gmail.com>
2019-12-29 22:50:36 +0000

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,65 @@
 |
 */
 
+Route::group(['prefix' => 'manage', 'middleware' => ['auth', 'admin']], function () {
+
+    Route::post('product-variants', 'Admin\ProductController@variants');
+
+
+
+    Route::get('products', 'Admin\ProductController@index');
+
+    Route::get('products/details/create', 'Admin\ProductController@createProductDetails');
+    Route::post('products/details', 'Admin\ProductController@storeProductDetails');
+    
+    Route::get('products/{product_id}/images/create', 'Admin\ProductController@createProductImages');
+    Route::post('products/{product_id}/images', 'Admin\ProductController@storeProductImages');
+    
+    Route::get('products/{product_id}/variants/create', 'Admin\ProductController@createProductVariants');
+    Route::post('products/{product_id}/proceed-to-variants', 'Admin\ProductController@proceedToProductVariants');
+    Route::post('products/{product_id}/variants', 'Admin\ProductController@storeProductVariants');
+    
+    Route::get('products/{product_id}/availability/create', 'Admin\ProductController@createProductAvailability');
+    Route::post('products/{product_id}/availability', 'Admin\ProductController@storeProductAvailability');
+    
+
+
+    Route::resource('administrators', 'Admin\AdministratorController');
+    //Route::resource('products', 'Admin\ProductController');
+    Route::resource('customers', 'Admin\CustomerController');
+
+    Route::get('/', 'Admin\DashboardController@index');
+
+
+
+
+
+});
+
+Route::middleware(['auth'])->group(function () {
+
+
+  
+
+
+    Route::get('/profile', function () {
+        echo 'user';
+    });
+
+
+    Route::get('/', function () {
+        return 'logged in';
+    });
+
+
+
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
